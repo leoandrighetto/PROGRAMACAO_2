0a1ae66d0631ad19c02787f1acc116389cb09c55
leoandrighetto <leoandrighetto02@gmail.com>
2025-02-17 15:13:28 -0300

    Update 002 APNP DE NIVELAMENTO.py

--- 002 APNP DE NIVELAMENTO.py
+++ 002 APNP DE NIVELAMENTO.py
@@ -1,15 +1,18 @@
 import sys
 
 def menu():
-    print()
-    print("F1 = CADASTRO")
-    print("F2 = INFORMAÇÕES")
-    print('F3 = MODIFICAR NOTA DO ALUNO')
-    print('F4 = SAIR DO SISTEMA')
-    print()
+
+    print(15* '-=')
+    print("F.1 = CADASTRO DE ALUNO")
+    print("F.2 = INFORMAÇÕES DE ALUNOS")
+    print('F.3 = MODIFICAR NOTA DE ALUNO')
+    print('F.4 = MÉDIA DAS NOTAS DOS ALUNOS')
+    print('F.0 = SAIR DO SISTEMA')
+    print(15* '-=')
+
     while True:
 
-        per = input('Digite a função desejada (ex: f1): ')
+        per = input('Digite a função desejada (ex: "f1"): ')
 
         if per.lower()=='f1':
             cadastro()
@@ -20,7 +23,10 @@ def menu():
         elif per.lower()=='f3':
             modNotas()
 
-        elif per.lower()=='f4':
+        elif per.lower() =='f4':
+            media()
+
+        elif per.lower()=='f0':
             print()
             print('O SISTEMA FOI ENCERRADO!')
             sys.exit()
@@ -31,34 +37,39 @@ alunos = []
 
 def cadastro():
 
-    
     while True:
 
         aluno = []
 
         while True:
-            nome = input('Digite o nome do aluno: ')
-            if not isinstance(nome, str):
-                print('DADOS INVÁLIDOS. ')
+            nome = str(input('Digite o nome do aluno: '))
+            if not nome.replace(' ','').isalpha():
+                print('DADOS INVÁLIDOS. Os nomes só podem conter letras')
             else:
                 aluno.append(nome)
                 break
 
-            nota = float(input('Digite a nota do aluno: '))
-
-            aluno.append(nota)
-
+        while True:
+            try:
+                nota = float(input('Digite a nota do aluno: '))
+                if nota >=0 and nota <= 10:
+                    aluno.append(nota)
+                    break
+                else:
+                    print('DADOS INVÁLIDOS. Digite apenas números de 1 a 10')
+            except ValueError:
+                print('DADOS INVÁLIDOS.Digite apenas números de 1 a 10')
 
         alunos.append(aluno)
-        while True:
-            p = input('Deseja adicionar mais alunos(S/N)? ')
 
-            if p.lower() == "n":
-                menu()
-            elif p.lower() == 's':
-                cadastro()
-            else:
-                print('OPÇÃO INVÁLIDA')
+
+        per = input('Deseja adicionar mais alunos(S/N)? ')
+
+        if per.lower() == "n":
+            menu()
+            break
+        elif per.lower() != "s":
+            print('OPÇÃO INVÁLIDA')
 
 
 def informacoes():
@@ -71,10 +82,16 @@ def informacoes():
             menu()
         
     else:
-        b = 000        
-        for i,a in alunos:
-            print (f'ALUNO: {i} | MATRÍCULA: {b} | NOTA {a} ')
-            b+= 1
+        for i, a in enumerate(alunos):
+            #FOR NUMERAÇÃO , ALUNO->(SUBLISTA)<  IN ENUMERATE(LISTA):
+            #ALUNOS = [[NOME,NOTA],[NOME,NOTA]]
+            
+            print (f'NOME: {a[0]} | MATRÍCULA: {i} | NOTA {a[1]} ')
+
+            #a[0] = o índice 0 na sublista aluno é o NOME.
+            #i = é uma iteração que começa no número 0 (MATRÍCULA)
+            #a[1] = o índice 1 na sublista aluno é a NOTA.
+
         while True:    
             per = input('Deseja cadastrar um novo aluno (S/N)? ')
             if per.lower()=='s':
@@ -85,4 +102,58 @@ def informacoes():
                 print('OPÇÃO INVÁLIDA!')
 
 
+def modNotas():
+    if not alunos:
+        print('NÃO EXISTEM ALUNOS CADASTRADOS!')
+        per = input('Deseja cadastrar um aluno (S/N)? ')
+        if per.lower() =='s':
+            cadastro()
+        else:
+            menu()
+    else:
+        
+        while True:
+            matricula = int(input('Nº da matrícula: '))
+            try:
+                nova = float(input('Nova  nota: '))
+
+                if nova >=0 and nova <= 10:
+                    alunos[matricula][1] = nova
+
+                    print('NOTA ATUALIZADA!')
+
+                    while True:
+                        per = str(input('Deseja atualizar mais notas(S/N)? '))
+                        if per.lower() == 'n':
+                            menu()
+                        elif per.lower() != 's':
+                            print('OPÇÃO INVÁLIDA!')
+                        else:
+                            break
+                   
+                else:
+                    print('DADOS INVÁLIDOS. Digite apenas números de 1 a 10')
+
+            except ValueError:
+                print('DADOS INVÁLIDOS.Digite apenas números de 1 a 10')
+
+def media():
+    if not alunos:
+        print('NÃO EXISTEM ALUNOS CADASTRADOS!')
+        per = input('Deseja cadastrar um aluno (S/N)? ')
+        if per.lower() =='s':
+            cadastro()
+        else:
+            menu()
+    
+    else:
+        soma = 0
+        for i in alunos:
+            soma+=i[1]
+
+        cal = soma/len(alunos)
+        print(f'MÉDIA DOAS ALUNOS = {cal:.2f}')
+
+
+        
 menu()
