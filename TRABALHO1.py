93b4d94f2cfe2b006348298a9aa3a5ebefd4d02f
leoandrighetto <leoandrighetto02@gmail.com>
2025-03-18 12:06:31 -0300

    Update TRABALHO1.py

--- TRABALHO1.py
+++ TRABALHO1.py
@@ -1,49 +1,91 @@
-class Livros:
-    def __init__(self, titulo, codigo, editora, area, ano, valor, quantidade_em_estoque):
-        self.titulo=titulo
-        self.codigo = codigo
-        self.editora = editora
-        self.area = area
-        self.ano = ano
-        self.valor = valor
-        self.quantidade_em_estoque = quantidade_em_estoque
+import sys
+class SistemadDeCadastros:
+    def __init__(self):
+        self.livros = {"codigo":{"a":'a','b':'b','c':'c','d':'d','e':'e','f':'f','g':'g'}}
 
-    def CadastroDeLivros(self,Livros):
+    def info(self):
+        for i, a in self.livros.items():
+            print(f"{i} - {a}")
+
+    def MenuInterativo(self, cadastro, listagem, busca_nome, busca_categoria, busca_valor, valor_estoque, encerrar):
+        print()
         print(10 * "-=")
-        print("CADASTRO DE LIVRO")
+        print("--MENU PRINCIPAL--")
+        print()
+        print("DIGITE A AÇÃO DESEJADA (Ex.: 1)")
+        print()
+        print("> 1. CADASTRAR UM NOVO LIVRO")
+        print("> 1. CADASTRAR UM NOVO LIVRO")
+        print("> 1. CADASTRAR UM NOVO LIVRO")
+        print("> 6. BUSCAR LIVRO POR CATEGORIA")
+        print("> 7. BUSCA LIVRO POR PREÇO DE LIVRO")
+        print("> 8. BUSCA POR QUANTIDADE EM ESTOQUE")
+        print("> 9. VALOR TOTAL EM ESTOQUE")
+        print("> 0. ENCERRAR ATIVIDADES")
+
+    def CadastrarNovoLivro(self):
+        print()
+        print(10 * "-=")
+        print("--CADASTRO DE LIVROS--")
         print()
 
-        registro_de_livros = {}
-
         while True:
+            novo_livro = {}
 
-            self.livro={}
+            codigo = input("> DIGITE O CÓDIGO DO LIVRO: ")
 
-            codigo = int(input("Informe o código do Livro: "))
-            livro.update({"codigo": codigo})
-            titulo = input("Informe o título do Livro: ")
-            livro.update({"titulo":titulo})
-            editora = input("Informe a editora  do Livro: ")
-            livro.update({"editora":editora})
-            categoria = input("Informe a categoria do Livro: ")
-            livro.update({"categoria":categoria})
-            valor = int(input("Informe o valor do Livro: "))
-            livro.update({"valor": valor})
-            quantidade_em_estoque = int(input("Informe a quantidade em estoque do Livro: "))
-            livro.update({"quantidade_em_estoque":quantidade_em_estoque})
+            titulo = input("> DIGITE O TÍTULO DO LIVRO: ")
+            novo_livro["Título"] = titulo
 
-            registro_de_livros[codigo]=self.livro
+            editora = input("> DIGITE A EDITORA DO LIVRO: ")
+            novo_livro["Editora"] = editora
 
-            print()
-            interação_1 = input("Deseja cadastar mais livros (S/N)? ")
+            categoria = input("> DIGITE A CATEGORIA DO LIVRO: ")
+            novo_livro['Categoria'] = categoria
 
-            if interação_1.lower() == "n":
-                break
+            ano = input('> DIGITE O ANO DO LIVRO: ')
+            novo_livro['Ano'] = ano
 
-        for i,a in registro_de_livros.items():
-            print(f"{i}-{a}")
+            valor = input('> DIGITE O VALOR DO LIVRO: ')
+            novo_livro['Valor'] = valor
 
-Livros.CadastroDeLivros(Livros)
+            quantidade_em_estoque = input('DIGITE A QUANTIDADE EM ESTOQUE DO LIVRO: ')
+            novo_livro['Quantidade em Estoque'] = quantidade_em_estoque
+
+            self.livros[codigo] = novo_livro
+
+            for i, a in self.livros.items():
+                print(f'>>>>CÓDIGO: {i}')
+                for b,c in a.items():
+                    print(f'{b}: {c}')
+                print()
 
 
+            # while True:
+            #     print(f"Código: {self.livros[codigo]}\nTítulo: {self.livros['titulo']}\n"
+            #           f"Editora: {self.livros['editora']}\nCategoria: {self.livros['categoria']}\n"
+            #           f"Ano: {self.livros['ano']}\nValor: {self.livros[valor]}\n"
+            #           f"Quantidade Em Estoque: {self.livros['quantidade_em_estoque']}")
 
+
+    # def ListarLivros(self):
+    #     pass
+    #
+    # def BuscaPorNome(self):
+    #     pass
+    #
+    # def BuscaPorCategoria(self):
+    #     pass
+    #
+    # def BuscaPorPreco(self):
+    #     pass
+    #
+    # def ValorTotalEmEstoque(self):
+    #     pass
+    #
+    # def EncerrarAtividades(self):
+    #     pass
+
+sistema = SistemadDeCadastros()
+
+sistema.CadastrarNovoLivro()
